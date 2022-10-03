c74c6651a618e61be190fd39006aa3a1cac56340
Ascetholix <sangak@mail.ru>
2022-10-03 23:45:32 +0300

    Add pattern001

--- pattern001/Program.cs
+++ pattern001/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Паттерн 000: 2х мерный массив строчный (string) конвертацией из int в string
+Console.Clear();
+
+void PrintArray(string[,] arg)
+{
+  for (int i = 0; i < arg.GetLength(0); i++)
+  {
+    for (int j = 0; j < arg.GetLength(1); j++)
+    {
+      Console.Write($"{arg[i, j]} ");
+    }
+    Console.WriteLine();
+  }
+}
+
+void FillArray(string[,] arg)
+{
+  for (int i = 0; i < arg.GetLength(0); i++)
+  {
+    for (int j = 0; j < arg.GetLength(1); j++)
+    {
+      arg[i, j] =Convert.ToString(i+1);    // 2 варианта
+      // arg[i, j] =Convert.ToString(j+1);
+    }
+  }
+}
+
+Console.Write("Введите длину строки: ");
+int rows =Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите длину столбца: ");
+int cols =Convert.ToInt32(Console.ReadLine());
+
+string[,] arr = new string[rows,cols];
+
+FillArray(arr);
+PrintArray(arr);
